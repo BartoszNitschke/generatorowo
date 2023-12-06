5e7fcf87826621e6cbf6a3010625b867e623497a
BartoszNitschke <s25771@pjwstk.edu.pl>
2023-12-06 18:06:18 +0100

    Project setup
    Created project with React&TailwindCSS configured

---
+++ src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-center font-bold text-yellow-500 text-[36px]">
+        GENERATOROWO
+      </h1>
+    </div>
+  );
+}
+
+export default App;
