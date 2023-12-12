ee56b67086d556ad33dbc5aee2989d38b3ea2d21
BartoszNitschke <s25771@pjwstk.edu.pl>
2023-12-12 21:32:53 +0100

    Small fix
    Fixed font size and navbar height

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,13 +6,13 @@ const Home = () => {
   return (
     <div className="w-full h-screen flex items-center bg-gradient-to-r from-[#000300] to-[#292929]">
       <div className="w-[55%] h-screen text-white flex flex-col items-center justify-center">
-        <h1 className="text-[64px] text-[#ebdb04] font-bold">
+        <h1 className="text-[60px] text-[#ebdb04] font-bold">
           ZASIL SIĘ JUŻ DZIŚ!
         </h1>
-        <p className="text-[36px] mt-12 ">
+        <p className="text-[32px] mt-12 ">
           Indywidualne podejście do każdego klienta
         </p>
-        <p className="text-[36px] mt-6 ">
+        <p className="text-[32px] mt-6 ">
           Zainstaluj generator na{" "}
           <TypeAnimation
             sequence={["DOMU", 1000, "GARAŻU", 1000, "FIRMIE", 1000]}
@@ -22,15 +22,15 @@ const Home = () => {
             className="text-[#ebdb04] font-semibold"
           />
         </p>
-        <button className="mt-12 bg-[#ebdb04] text-[28px] px-16 py-3 rounded-xl text-gray-900 font-semibold shadow-2xl shadow-gray-400 hover:scale-105 focus:outline-none">
+        <button className="mt-12 bg-[#ebdb04] text-[28px] px-16 py-3 rounded-xl text-gray-900 font-semibold shadow-lg shadow-gray-400 hover:scale-105 focus:outline-none">
           Katalog
         </button>
       </div>
-      <div className="w-[45%] mt-[100px]">
+      <div className="w-[45%] mt-[80px]">
         <img
           src={GeneratorPhoto}
           alt=""
-          className="w-full h-screen object-cover rounded-l-[300px]"
+          className="w-full h-screen object-cover rounded-l-[200px]"
         />
       </div>
     </div>
--- src/components/MyPower.jsx
+++ src/components/MyPower.jsx
@@ -3,6 +3,7 @@ import React from "react";
 const MojeZasilanie = () => {
   return (
     <div name="moje-zasilanie" className="w-full h-screen">
+      <div className="h-[40px] bg-gradient-to-r from-[#000300] to-[#292929]"></div>
       <p>Moje zasilanie</p>
     </div>
   );
