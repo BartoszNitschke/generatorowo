b00c59eed7e57bb8a57824fe7abf357b8dc35b7c
BartoszNitschke <s25771@pjwstk.edu.pl>
2024-02-18 14:22:05 +0100

    added environemental variables

--- src/lib/firebaseConfig.js
+++ src/lib/firebaseConfig.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC40Kmh_KQuG-4kpEvOfLDDdjIpk4A1pO4",
-  authDomain: "generatorowo-cb4ab.firebaseapp.com",
-  projectId: "generatorowo-cb4ab",
-  storageBucket: "generatorowo-cb4ab.appspot.com",
-  messagingSenderId: "476133980859",
-  appId: "1:476133980859:web:6f4efae73a76ada96ac79e",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
